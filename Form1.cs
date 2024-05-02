22bef9da036fc0131ed6fd63a99db0c15d5c4cc0
Licenciaturas <Licenciaturas@L301-M22>
2024-05-02 11:42:23 -0600

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace herencia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void suma_Click(object sender, EventArgs e)
+        {
+            //voy a darle comportamiento a mi boton
+            //voy a utilizar mi clase hija sumar 
+            Sumar suma = new Sumar(); //instancie un nuevo objeto suma llamar al metodo de clase suma operar 
+
+            textBox3.Text = suma.operar(int.Parse(textBox1.Text), int.Parse(textBox2.Text)).ToString();
+        }
+
+        private void resta_Click(object sender, EventArgs e)
+        {
+            //voy a darle comportamiento a mi boton
+            //voy a utilizar mi clase hija sumar 
+            Restar resta = new Restar(); //instancie un nuevo objeto suma llamar al metodo de clase resta operar 
+
+            textBox3.Text = resta.operar(int.Parse(textBox1.Text), int.Parse(textBox2.Text)).ToString();
+        }
+
+        private void Mulriplicar_Click(object sender, EventArgs e)
+        {
+            //voy a darle comportamiento a mi boton
+            //voy a utilizar mi clase hija sumar 
+            Multiplicar multiplicar = new Multiplicar(); //instancie un nuevo objeto suma llamar al metodo de clase suma operar 
+
+            textBox3.Text = multiplicar.operar(int.Parse(textBox1.Text), int.Parse(textBox2.Text)).ToString();
+        }
+
+        private void Limpa_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        private void salir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void dividir_Click(object sender, EventArgs e)
+        {
+            //voy a darle comportamiento a mi boton
+            //voy a utilizar mi clase hija sumar 
+            Division division = new Division(); //instancie un nuevo objeto suma llamar al metodo de clase suma operar 
+
+            textBox3.Text = division.operar(int.Parse(textBox1.Text), int.Parse(textBox2.Text)).ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace herencia
+{
+    class operacion
+    {
+        //declaracion de variables que usa la clase
+        //tipo de acceso        tipo de dato          nombre de dato
+       
+        protected               int                     valor1;
+        protected               int                      valor2;
+        protected               int                     resultado;
+
+
+     
+        //comentsario desde web
+
+        //metodos o funciones de operacion
+        
+
+        public int Valor1 //CAJA DE MEMORIA 1
+        {
+            get { return valor1; } //vamos por el valor de la caja de texto
+
+            set { valor1 = value; } //lo guardamos en memoria 
+        }
+
+        public int Valor2 //CAJA DE MEMORIA 2
+        {
+            get { return valor2; } //vamos por el valor de la caja de texto
+
+            set { valor2 = value; } //lo guardamos en memoria 
+        }
+
+        public int Resultado //CAJA DE MEMORIA 1
+        {
+            get { return resultado; } //vamos por el valor de la caja de texto
+
+            set { resultado = value; } //lo guardamos en memoria 
+        }
+
+    }
+
+    //creamos nuestras clases hijos que usaran los metodos de crear dato
+    
+    class Sumar : operacion //los : nos dan permiso de usar lo que tiene operacion
+    {
+        //las clases hijas tambien pueden tener sus propios atributos y métodos pueden tener variables 
+        //de uso solamente para la clase hijo
+        //declarar variables de esta clase 
+        //atributos de esta clase 
+        //metodos de clase 
+
+        public int operar(int v1, int v2)
+        {
+            valor1 = v1; //accedemos al constructor de operación
+            valor2 = v2;
+            return resultado = valor1 + valor2;
+        }
+    }
+
+    class Restar : operacion //los : nos dan permiso de usar lo que tiene operacion
+    {
+        //las clases hijas tambien pueden tener sus propios atributos y métodos pueden tener variables 
+        //de uso solamente para la clase hijo
+        //declarar variables de esta clase 
+        //atributos de esta clase 
+        //metodos de clase 
+
+        public int operar(int v1, int v2)
+        {
+            valor1 = v1; //accedemos al constructor de operación
+            valor2 = v2;
+            return resultado = valor1 - valor2;
+        }
+    }
+
+    class Multiplicar : operacion //los : nos dan permiso de usar lo que tiene operacion
+    {
+        //las clases hijas tambien pueden tener sus propios atributos y métodos pueden tener variables 
+        //de uso solamente para la clase hijo
+        //declarar variables de esta clase 
+        //atributos de esta clase 
+        //metodos de clase 
+
+        public int operar(int v1, int v2)
+        {
+            valor1 = v1; //accedemos al constructor de operación
+            valor2 = v2;
+            return resultado = valor1 * valor2;
+        }
+    }
+
+    class Division : operacion //los : nos dan permiso de usar lo que tiene operacion
+    {
+        //las clases hijas tambien pueden tener sus propios atributos y métodos pueden tener variables 
+        //de uso solamente para la clase hijo
+        //declarar variables de esta clase 
+        //atributos de esta clase 
+        //metodos de clase 
+
+        public int operar(int v1, int v2)
+        {
+            valor1 = v1; //accedemos al constructor de operación
+            valor2 = v2;
+            return resultado = valor1 / valor2;
+        }
+    }
+    //esto es un comentario nuevo 26 de abril
+    //esto es un segunod comentario nuevo 26 de abril
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
